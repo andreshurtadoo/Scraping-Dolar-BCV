4ee2625e155ce063b6b4342af9e2f02f54451e90
Nechy <hurtadoandres73@hotmail.com>
2023-04-18 14:07:35 -0400

    scrapping readi

---
+++ index.js
@@ -0,0 +1,29 @@
+const PORT = 8080
+const URL = 'https://www.bcv.org.ve/'
+
+const https = require('https');
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+const app = express()
+
+const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+})
+
+axios.get(URL, {httpsAgent})
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        $('#dolar', html).each(function() {
+            const dolar = $(this).find('strong').text()
+            console.log(dolar);
+        })
+    })
+
+
+app.listen(PORT, () => {
+    console.log(`
+        Listening on http://127.0.0.1:${PORT}  
+    `)
+})
